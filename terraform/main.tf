9d9548e541fbb32e323c92f4fad06e40d60a322d
Lester Guerzon <guerzon@proton.me>
2025-07-27 16:10:28 +0800

    Terraform objects changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,23 @@
-# module "vpc" {
-#   source = "guerzon/gcp/modules//network"
-#   version = "1.0.0"
-#   environment = var.environment
-# }
-# 
-# module "subnet" {
-#   source = "guerzon/gcp/modules//subnets"
-#   version = "1.0.0"
-#   network = module.vpc.network
-#   subnets = [
-#     {
-#       name       = "singapore-subnet"
-#       region     = var.region
-#       cidr_range = "10.0.0.0/16"
-#     }
-#   ]
-# }
+module "vpc" {
+  source      = "guerzon/gcp/modules//network"
+  version     = "1.0.0"
+  environment = var.environment
+}
+
+module "subnet" {
+  source  = "guerzon/gcp/modules//subnets"
+  version = "1.0.0"
+  network = module.vpc.network
+  subnets = [
+    {
+      name       = "singapore-subnet"
+      region     = var.region
+      cidr_range = "10.0.0.0/16"
+    }
+  ]
+}
 
 # module "dns_zone_private" {
-#   # source      = "git::https://github.com/guerzon/terraform-modules-gcp//dnszone"
 #   source           = "../../../Google-Cloud/terraform-modules-gcp/dnszone"
 #   zone_name        = "hideout"
 #   zone_dns_name    = var.private_zone_dns
@@ -31,22 +30,4 @@
 #   visible_gke_clusters = [
 #     module.gke.cluster_id
 #   ]
-# }
-
-# module "router" {
-#   # source      = "git::https://github.com/guerzon/terraform-modules-gcp//router"
-#   source      = "../../../Google-Cloud/terraform-modules-gcp/router"
-#   router_name = var.router_name
-#   network     = module.vpc.network_name
-#   region      = var.region
-#   bgp_asn     = 64600
-# }
-
-# module "nat_gateway" {
-#   # source      = "git::https://github.com/guerzon/terraform-modules-gcp//natgateway"
-#   source           = "../../../Google-Cloud/terraform-modules-gcp/natgateway"
-#   nat_gateway_name = "homelab-natgateway"
-#   router_name      = var.router_name
-#   region           = var.region
-#   network_tier     = "STANDARD"
 # }
